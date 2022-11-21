b08bbed2d03867975d7e2973e87fbcb9abd84eac
Cale-Torino <cale@techrad.co.za>
2022-11-21 20:44:36 +0200

    added JS, PHP and CURL example

--- b315/curl.php
+++ b315/curl.php
@@ -1,6 +1,17 @@
 <?php
-//C:\Software\xampp\htdocs\Tuya\ELAC_testToken.php
-//http://127.0.0.1/b315/curl.php?data=GET
+/**
+ * curl.php
+ *
+ * Send POST & GET requests via cURL
+ *
+ * @category   cURL requests
+ * @package    cURL
+ * @author     C.A Torino
+ * @version    V1.0.0
+ * @since      2022 November 21
+ **/
+//http://127.0.0.1/b315/curl.php?data=POST&url=http://192.168.8.102/goform/sysReboot&postdata=data&cookie=data
+//http://127.0.0.1/b315/curl.php?data=GET&url=http://192.168.8.102/goform/getHomePageInfo?random=0.7189040724881826&modules=loginAuth,wifiRelay&cookie=data
 
 //http://192.168.8.1/api/user/state-login
 //http://192.168.8.1/api/webserver/SesTokInfo
@@ -38,7 +49,23 @@ $tokinfo = base64_decode($tokinfo);
 //foo=bar;baz=foo
 //$cookie = "SessionID=UM4ONXDhBOHOfMHrMRm5859o7nslK+IM8iKSjYzHuzRqpeqZH62OOBlc31+Gk/6Nqh3rp6frYI0Q50Q7S1zeCl4elJrXd0KRDxdGR7WpmkiUGrSMiWT9JPMDPhPR6fQG";
 
-$headers = array();
+$header1 = array();
+$header2 = array();
+
+function HandleHeaderLine($curl, $headerLine)
+{
+  global $header1;
+  global $header2;
+  if (preg_match('/^Set-Cookie:\s*([^;]*)/mi', $headerLine, $c) == 1)
+  {
+    $header1["SetCookie"] = $c[1];
+  }
+  if (preg_match('/^__RequestVerificationToken:\s*([^;]*)/mi', $headerLine, $c) == 1)
+  {
+    $header2["RequestVerificationToken"] = $c[1];
+  }
+  return strlen($headerLine);
+}
 
 if($data == "POST")
 {
@@ -52,63 +79,12 @@ else
 {
     echo json_encode(array(
         "Error" => "1",
-        "Message" => "No Command provided"
+        "Message" => "No request provided"
       ));
 }
 
-function xml_encode(mixed $value=null, string $key="root", SimpleXMLElement $parent=null){
-  if(is_object($value)) $value = (array) $value;
-  if(!is_array($value)){
-      if($parent === null){   
-          if(is_numeric($key)) $key = 'item';             
-          if($value===null) $node = new SimpleXMLElement("<$key />");//$node = new SimpleXMLElement("<$key />");
-          else              $node = new SimpleXMLElement("<$key>$value</$key>");
-      }
-      else{
-          $parent->addChild($key, $value);
-          $node = $parent;
-      }
-  }
-  else{
-      $array_numeric = false;
-      if($parent === null){ 
-          if(empty($value)) $node = new SimpleXMLElement("<$key />");//$node = new SimpleXMLElement("<$key />");
-          else              $node = new SimpleXMLElement("<$key></$key>");
-      }
-      else{
-          if(!isset($value[0])) $node = $parent->addChild($key);
-          else{
-              $array_numeric = true;
-              $node = $parent;
-          }
-      }
-      foreach( $value as $k => $v ) {
-          if($array_numeric) xml_encode($v, $key, $node);
-          else xml_encode($v, $k, $node);
-      }
-  }       
-  return $node;
-}
-
 //https://stackoverflow.com/questions/9183178/can-php-curl-retrieve-response-headers-and-body-in-a-single-request/25118032#25118032
-function HandleHeaderLine($curl,$header_line){
-  //$header = explode(':', $header_line);
-  //$result = "";
-  //foreach ($header as $value) {
-    //$result .= $value;
-  //}
-  //echo $result;
-  // echo json_encode(array(
-  //   "result" => "1",
-  //   "Message" => $header_line
-  // ));
-  global $headers;
-  $headers[] = $header_line;
-  //echo $header_line; // or do whatever
-  return strlen($header_line);
-  //return $header_line;
-}
-//cUrl Get function
+
 function cURLGET($url,$cookie){
   $curl = curl_init();
   curl_setopt_array($curl, array(
@@ -118,8 +94,8 @@ function cURLGET($url,$cookie){
     CURLOPT_MAXREDIRS => 10,
     CURLOPT_COOKIE => $cookie,
     CURLOPT_TIMEOUT => 0,
-    CURLOPT_HEADERFUNCTION => "HandleHeaderLine",
-    CURLOPT_FOLLOWLOCATION => true,
+    //CURLOPT_HEADERFUNCTION => "HandleHeaderLine",
+    CURLOPT_FOLLOWLOCATION => false,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "GET",
     CURLOPT_HTTPHEADER => array(
@@ -134,6 +110,9 @@ function cURLGET($url,$cookie){
 }
 
 function cURLPOST($url,$postdata,$cookie,$tokinfo){
+  global $header1;
+  global $header2;
+
 $curl = curl_init();
 //$postdata = json_encode($data);
 curl_setopt_array($curl, array(
@@ -145,7 +124,7 @@ curl_setopt_array($curl, array(
   CURLOPT_POSTFIELDS => $postdata,
   CURLOPT_TIMEOUT => 0,
   CURLOPT_HEADERFUNCTION => "HandleHeaderLine",
-  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_FOLLOWLOCATION => false,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
   CURLOPT_HTTPHEADER => array(
@@ -157,8 +136,18 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 
 curl_close($curl);
-echo $response;
-//global $headers;
-//$xml_element = xml_encode($headers, "headerline");
-//echo $xml_element->asXML();
+
+if($header1)
+{
+  $string = preg_replace('~[\r\n]+~', '', $header1);
+  $j = json_encode($string);
+  header("SetCookie: $j");
 }
+if($header2)
+{
+  $string = preg_replace('~[\r\n]+~', '', $header2);
+  $j = json_encode($string);
+  header("RequestVerificationToken: $j");
+}
+echo $response;
+} 
